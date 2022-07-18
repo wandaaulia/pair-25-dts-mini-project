b8ae1a2d4dd7531cf8e3500af56731223b63abf1
wanda <wandaul16@gmail.com>
2022-07-18 20:19:49 +0700

    add login and register

---
+++ src/config/firebase.js
@@ -0,0 +1,20 @@
+import { initializeApp } from 'firebase/app';
+import { getAuth,  GoogleAuthProvider} from 'firebase/auth';
+
+
+const firebaseConfig = {
+  apiKey: "AIzaSyAKqhVyehGHMTGS1TkKeILqYUsmOZIy4f0",
+  authDomain: "movie-react-26fbf.firebaseapp.com",
+  projectId: "movie-react-26fbf",
+  storageBucket: "movie-react-26fbf.appspot.com",
+  messagingSenderId: "258904494009",
+  appId: "1:258904494009:web:1dedf400a54e8a5175b068"
+};
+
+// Initialize Firebase
+const app = initializeApp(firebaseConfig);
+const auth = getAuth(app);
+export const provider = new GoogleAuthProvider();
+
+
+export { auth };
