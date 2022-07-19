208eb4e240f83dcdd9d2c43494bfa2e708c64328
akbarism <akbarismaiel@gmail.com>
2022-07-19 09:00:06 +0700

    add(package): swiper js

--- src/components/base/RegularListMovie.jsx
+++ src/components/base/RegularListMovie.jsx
@@ -1,10 +1,17 @@
 import React, { useEffect, useState } from 'react'
 import { useDispatch, useSelector } from 'react-redux'
 import { fetchApi } from '../../store/reducer.js'
+
+import { Swiper, SwiperSlide } from 'swiper/react';
+import "swiper/css";
+import "swiper/css/navigation";
+import { Navigation } from "swiper";
+
 const RegularListMovie = (props) => {
     const dispatch = useDispatch()
 
     const staticState = useSelector(state => state.apis)
+    
     const [movie, setMovie] = useState([])
     const fetchData = async () => {
         try {
@@ -25,15 +32,19 @@ const RegularListMovie = (props) => {
         <div style={{ padding: '30px 0 30px 20px' }}>
             <p className="title_default">{props.title}</p>
             <div className='temp_list_movie'>
-                {
-                    movie.map((item, i) => {
-                        return (
-                            <div className="item_movie color_default" key={i}>
-                                <img className='item_movie' src={`${staticState.host_img}${item.poster_path}`} alt="" />
-                            </div>
-                        )
-                    })
-                }
+                <Swiper id="main" navigation={true} slidesPerView={5} modules={[Navigation]} className="mySwiper">
+                    {
+                        movie.map((item, i) => {
+                            return (
+                                <SwiperSlide key={i}>
+                                    <div className="item_movie color_default" >
+                                        <img className='item_movie' src={`${staticState.host_img}${item.poster_path}`} alt="" />
+                                    </div>
+                                </SwiperSlide>
+                            )
+                        })
+                    }
+                </Swiper>
             </div>
         </div>
     )
