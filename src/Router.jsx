6980dc1eb49d07b5146b5df142fc4ddf5884507f
akbarism <akbarismaiel@gmail.com>
2022-07-21 15:36:07 +0700

    dev(login): add linear gradient on cover auth

--- src/Router.jsx
+++ src/Router.jsx
@@ -6,26 +6,35 @@ import Home from "./components/Home";
 import ProtectedRoute from "./components/auth/ProtectedRoute";
 import PublicRoute from "./components/auth/PubliceRoute";
 import MovieDetail from "./components/MovieDetail";
-
+import { SplashScreen } from "./components/SplashScreen";
+import { useAuthState } from 'react-firebase-hooks/auth';
+import { auth } from '../src/config/firebase'
 
 const RouterSetup = () => {
-
-
-  return (
-    <Router>
-      <Routes>
-        <Route element={<PublicRoute loginOnly={true} />}>
-          <Route path="/login" element={<Login />} />
-          <Route path="/register" element={<Register />} />
-          <Route path="/select-profile" element={<SelectProfile />} />
-        </Route>
-        <Route element={<ProtectedRoute loginOnly={true} />}>
-          <Route path="/" element={<Home />} />
-          <Route path="/movie/:id" element={<MovieDetail />} />
-        </Route>
-      </Routes>
-    </Router>
-  );
+  // eslint-disable-next-line
+  const [user, loading] = useAuthState(auth);
+  console.log(loading)
+  if (loading) {
+    return (
+      <SplashScreen />
+    )
+  } else {
+    return (
+      <Router>
+        <Routes>
+          <Route element={<PublicRoute loginOnly={true} />}>
+            <Route path="/login" element={<Login />} />
+            <Route path="/register" element={<Register />} />
+            <Route path="/select-profile" element={<SelectProfile />} />
+          </Route>
+          <Route element={<ProtectedRoute loginOnly={true} />}>
+            <Route path="/" element={<Home />} />
+            <Route path="/movie/:id" element={<MovieDetail />} />
+          </Route>
+        </Routes>
+      </Router>
+    );
+  }
 }
 
 export default RouterSetup;
