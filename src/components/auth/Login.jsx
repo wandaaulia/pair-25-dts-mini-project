a9f95f3a6c35901e8136f11d7605639c82f93763
akbarism <akbarismaiel@gmail.com>
2022-07-21 15:56:58 +0700

    add(asset): add asset 6-10

--- src/components/auth/Login.jsx
+++ src/components/auth/Login.jsx
@@ -1,21 +1,21 @@
 import React, { useState } from 'react';
-import Avatar from '@mui/material/Avatar';
+// import Avatar from '@mui/material/Avatar';
 import Button from '@mui/material/Button';
 import CssBaseline from '@mui/material/CssBaseline';
 import TextField from '@mui/material/TextField';
-import FormControlLabel from '@mui/material/FormControlLabel';
-import Checkbox from '@mui/material/Checkbox';
+// import FormControlLabel from '@mui/material/FormControlLabel';
+// import Checkbox from '@mui/material/Checkbox';
 import Grid from '@mui/material/Grid';
 import Box from '@mui/material/Box';
-import LockOutlinedIcon from '@mui/icons-material/LockOutlined';
+// import LockOutlinedIcon from '@mui/icons-material/LockOutlined';
 import Typography from '@mui/material/Typography';
-import Container from '@mui/material/Container';
+// import Container from '@mui/material/Container';
 import { Link, useNavigate } from 'react-router-dom';
 import Paper from '@mui/material/Paper';
 import { signInWithEmailAndPassword } from 'firebase/auth';
 import { signInWithPopup } from "firebase/auth";
 import { auth, provider } from '../../config/firebase'
-import { useDispatch, useSelector } from 'react-redux'
+import {  useSelector } from 'react-redux'
 
 import './login.css'
 
